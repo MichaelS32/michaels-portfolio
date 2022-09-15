9cc18795a46850ecadccb7e9ca06f57002dc73a4
mschneider <michaelschneider3232@gmail.com>
2022-09-15 18:57:31 -0500

    edited portfolio component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,44 +1,69 @@
 import React from 'react';
-import Grid from '@mui/material/Grid'; // Grid version 1
-import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 
 const projects = [
     {
         name: "text editor",
         src: "https://text-editor-pwa3232.herokuapp.com/",
-        github: "https://github.com/MichaelS32/text-editor"
+        github: "https://github.com/MichaelS32/text-editor",
+        img: "../assets/images/jate-screenshot.jpg",
+        alt: "screenshot of deployed text editor app"
     },
     {
         name: "date de-stresser",
         src: "https://yvettesalinas.github.io/date-destresser/",
-        github: "https://github.com/yvettesalinas/date-destresser"
+        github: "https://github.com/yvettesalinas/date-destresser",
+        img: "../assets/images/date-destresser-screenshot.jpg",
+        alt: "screenshot of date destresser app"
     },
     {
         name: "horiseon",
         src: "https://michaels32.github.io/Horiseon-Challenge-01/",
-        github: "https://github.com/MichaelS32/Horiseon-Challenge-01"
+        github: "https://github.com/MichaelS32/Horiseon-Challenge-01",
+        img: "../assets/images/horiseon-screenshot.jpg",
+        alt: "screenshot of deployed horiseon challenge website"
     },
     {
         name: "Java Script Quiz",
         src: "https://michaels32.github.io/js-quiz/",
-        github: "https://github.com/MichaelS32/js-quiz"
+        github: "https://github.com/MichaelS32/js-quiz",
+        img: "../assets/images/js-quiz-screenshot.jpg",
+        alt: "screenshot of deployed java script quiz app"
     }
 ]
 function Portfolio() {
     return (
         <section>
             {projects.map((project) => (
-                <div className="flex-row" key={project.name}>
-                    <div>
-                        <a href={project.src}>
-                            <h2>{project.name}</h2>
-                        </a>
-                        <a href={project.github}>
-                            <h3>Github Repo</h3>
-                        </a>
-                    </div>
-                </div>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={project.img}
+                            alt={project.alt}
+                        />
+                        <CardContent>
+                            <div key={project.name}>
+                                <div>
+                                    <a href={project.src}>
+                                        <h2>{project.name}</h2>
+                                    </a>
+                                    <a href={project.github}>
+                                        <h3>Github Repo</h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </CardActionArea>
+
+                </Card>
+
             ))}
         </section>
     )
