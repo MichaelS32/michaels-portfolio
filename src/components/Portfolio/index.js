837985804da22b99c7102970e83d8646eafa31ba
mschneider <michaelschneider3232@gmail.com>
2022-09-16 00:39:22 -0500

    a few more edits

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,53 +11,55 @@ const projects = [
         name: "text editor",
         src: "https://text-editor-pwa3232.herokuapp.com/",
         github: "https://github.com/MichaelS32/text-editor",
-        img: "../assets/images/jate-screenshot.jpg",
+        img: "../../../assets/images/jate-screenshot.jpg",
         alt: "screenshot of deployed text editor app"
     },
     {
         name: "date de-stresser",
         src: "https://yvettesalinas.github.io/date-destresser/",
         github: "https://github.com/yvettesalinas/date-destresser",
-        img: "../assets/images/date-destresser-screenshot.jpg",
+        img: "../../../assets/images/date-destresser-screenshot.jpg",
         alt: "screenshot of date destresser app"
     },
     {
         name: "horiseon",
         src: "https://michaels32.github.io/Horiseon-Challenge-01/",
         github: "https://github.com/MichaelS32/Horiseon-Challenge-01",
-        img: "../assets/images/horiseon-screenshot.jpg",
+        img: "../../../assets/images/horiseon-screenshot.jpg",
         alt: "screenshot of deployed horiseon challenge website"
     },
     {
         name: "Java Script Quiz",
         src: "https://michaels32.github.io/js-quiz/",
         github: "https://github.com/MichaelS32/js-quiz",
-        img: "../assets/images/js-quiz-screenshot.jpg",
+        img: "../../../assets/images/js-quiz-screenshot.jpg",
         alt: "screenshot of deployed java script quiz app"
+
     }
 ]
 function Portfolio() {
     return (
         <section>
             {projects.map((project) => (
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
+                <Card sx={{ maxWidth: 345 }} key={project.name}>
+                    <CardActionArea key={project.name}>
                         <CardMedia
                             component="img"
                             height="140"
                             image={project.img}
                             alt={project.alt}
+                            key={project.alt}
                         />
                         <CardContent>
                             <div key={project.name}>
-                                <div>
-                                    <a href={project.src}>
-                                        <h2>{project.name}</h2>
-                                    </a>
-                                    <a href={project.github}>
-                                        <h3>Github Repo</h3>
-                                    </a>
-                                </div>
+
+                                <a href={project.src} key={project.src}>
+                                    <h2 key={project.name}>{project.name}</h2>
+                                </a>
+                                <a key={project.github} href={project.github}>
+                                    <h3 key={project.github}>Github Repo</h3>
+                                </a>
+
                             </div>
                         </CardContent>
                     </CardActionArea>
