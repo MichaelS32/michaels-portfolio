b0c5e4382a3223b66a4a8b26fd77dcec4cbe5c98
MichaelsLaptop <michaelschneider3232@gmail.com>
2022-09-18 17:58:51 -0700

    transferring from laptop to desktop

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom'
 
 
 function categorySelected(name) {
@@ -19,43 +20,56 @@ function Nav(props) {
 
     return (
         <header>
-            <h2 className='logo'>
-                <a href="/" className='navbar-brand'>
-                    <span role="img" aria-label="hammer">🔨</span> Michael's Workshop
-                </a>
-            </h2>
+            <Link to='/home'>
+                <h2 className='logo'>
+                    <a href="/" className='navbar-brand'>
+                        <span role="img" aria-label="hammer">🔨</span> Michael's Workshop
+                    </a>
+                </h2>
+            </Link>
+            
             <nav>
                 <ul className="flex-row navbar has-shadow justify-content-around">
 
-
-                    <li className='nav'>
-                        <a className='nav' href="#about">
-                            <span className='nav' onClick={() => setContactSelected(false)}>
-                                About Me
-                            </span>
-                        </a>
-                    </li>
-                    <li className={`nav ${contactSelected && 'navActive'}`}>
-                        <a className='nav' href="#contact">
-                            <span className='nav' onClick={() => setContactSelected(true)}>
-                                Contact
-                            </span>
-                        </a>
-                    </li>
-                    <li className='nav'>
-                        <a className='nav' href="#portfolio">
-                            <span className='nav' onClick={() => setContactSelected(false)}>
-                                Portfolio
-                            </span>
-                        </a>
-                    </li>
-                    <li className='nav'>
-                        <a className='nav' href="#resume">
-                            <span className='nav' onClick={() => setContactSelected(false)}>
-                                Resume
-                            </span>
-                        </a>
-                    </li>
+                    <NavLink exact='true' activeClassname='active' to='/about'>
+                        <li className='nav'>
+                            <a className='nav' href="#about">
+                                <span className='nav' onClick={() => setContactSelected(false)}>
+                                    About Me
+                                </span>
+                            </a>
+                        </li>
+                    </NavLink>
+                    
+                    <NavLink exact='true' activeClassname='active' to='/contact'>
+                        <li className={`nav ${contactSelected && 'navActive'}`}>
+                            <a className='nav' href="#contact">
+                                <span className='nav' onClick={() => setContactSelected(true)}>
+                                    Contact
+                                </span>
+                            </a>
+                        </li>
+                    </NavLink>
+                    
+                    <NavLink exact='true' activeClassname='active' to='/portfolio'>
+                        <li className='nav'>
+                            <a className='nav' href="#portfolio">
+                                <span className='nav' onClick={() => setContactSelected(false)}>
+                                    Portfolio
+                                </span>
+                            </a>
+                        </li>
+                    </NavLink>
+                    
+                    <NavLink exact='true' activeClassname='active' to='resume'>
+                        <li className='nav'>
+                            <a className='nav' href="#resume">
+                                <span className='nav' onClick={() => setContactSelected(false)}>
+                                    Resume
+                                </span>
+                            </a>
+                        </li>
+                    </NavLink>
 
                 </ul>
             </nav>
